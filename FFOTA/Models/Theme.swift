c02a96d29ab1edda93544e7876e054aa610fced8
Yunji Kim <yunji.kmm@gmail.com>
2023-08-06 13:35:21 +0900

    add Color Theme file

---
+++ FFOTA/Models/Theme.swift
@@ -0,0 +1,30 @@
+
+import Foundation
+
+enum Theme: String, CaseIterable {
+    // 메인 컬러 테마
+    case ivory
+    case vanila
+    case strawberry
+    case cherry
+    
+    // 색상 팔레트
+    case red
+    case orange
+    case yellow
+    case green
+    case blue
+    case navy
+    case purple
+    case pink
+    
+    // 잔디 컬러
+    case one
+    case two
+    case three
+    case four
+    case five
+    
+    // 리스트 아이템, 시트 배경 컬러
+    case darkivory
+}
